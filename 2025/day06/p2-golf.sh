bff4735b64cb32641141bd6571a76d480dfc254e
Sarah Schulte <git@sarah.engineer>
2025-12-06 00:34:54 -0800

    more goofy sed

--- 2025/day06/p2-golf.sh
+++ 2025/day06/p2-golf.sh
@@ -1,4 +1,4 @@
 set -f;mapfile -t a;for((h=${#a[@]}-1;r<${#a};++r))
 do n=${a[h]:r:2};$n||o=$_;${n:1}||o=_;for((c=0;c<h;))
 do printf ${a[c++]:r:1};done;printf $o
-done|sed 's=. *_=+=g;s=$=0\n='|bc
+done|sed 's>. *_>+>g;s<$<0\n<'|bc
