63afbdd7e6a0094797c5579d7ed5c3f419868fcd
Sarah Schulte <git@sarah.engineer>
2025-12-07 02:14:38 -0800

    cleanup

--- 2025/day07/p2.sh
+++ 2025/day07/p2.sh
@@ -1,41 +1,16 @@
 #!/bin/bash
 
 FILE="$1"
-# FILE=input.txt
 
 mapfile -t grid < "$FILE"
 
 h=${#grid[@]}
 w=${#grid[0]}
 
-print_grid() {
-  local x y
-  local rx=$1
-  local ry=$2
-  local r=${3:-32}
-  for ((y=0; y<h; y++)); do
-    for ((x=0; x<w; x++)); do
-      if [[ x -eq rx && y -eq ry ]]; then
-        RESET_ME=1
-        echo -ne '\033['$r'm'
-      fi
-      if [[ -z ${cache[$x,$y]} ]]; then
-        printf ${grid[y]:x:1}
-      else
-        local cached=${cache[$x,$y]}
-        printf $((cached % 10))
-
-      fi
-      if [[ -n $RESET_ME ]]; then
-        unset RESET_ME
-        echo -ne '\033[39m'
-      fi
-    done
-    echo
-  done
-}
-
+declare -A cache
 q=()
+
+# find our starting point
 for ((y=0; y<h; y++)); do
   for ((x=0; x<w; x++)); do
     if [[ ${grid[y]:x:1} == S ]]; then
@@ -46,7 +21,6 @@ for ((y=0; y<h; y++)); do
     fi
   done
 done
-print_grid ${q[0]} $((q[1]-1)) 1>&2
 
 set_grid() {
   local x=$1
@@ -55,45 +29,33 @@ set_grid() {
   grid[y]=${grid[y]:0:x}$c${grid[y]:x+1}
 }
 
-declare -A cache
-
-count=1
 while [[ ${#q[@]} -gt 0 ]]; do
-  end=${#q[@]}
-  ((end-=3))
+  end=$((${#q[@]}-3))
   read x y z <<< "${q[end]} ${q[end+1]} ${q[end+2]}"
-  # read x y z <<< "${q[0]} ${q[1]} ${q[2]}"
   q=(${q[@]:0:end})
-  # q=(${q[@]:3})
+
   c=${grid[y]:x:1}
   cached=${cache[$x,$y]}
   if [[ $z == '|' ]]; then
+    # we are revisiting a beam; update cache
     below=${cache[$x,$((y+1))]}
     cache[$x,$y]=${below:-1}
-    # print_grid $x $y 35 1>&2
-    foo=0
   elif [[ $z == '^' ]]; then
+    # we are revisiting a splitter; update splitter
     left=${cache[$((x-1)),$y]}
     right=${cache[$((x+1)),$y]}
     cache[$x,$y]=$((left+right))
-    # print_grid $x $y 35 1>&2
-    foo=0
   elif [[ -n "$cached" ]]; then
-    continue
+    continue # i am speed
   elif [[ $c == '.' || $c == '|' ]]; then
     set_grid $x $y '|'
-    # print_grid $x $y 33 1>&2
-    q+=($x $y '|')
-    q+=($x $((y+1)) 'v')
+    q+=($x $y '|') # second, revisit ourself
+    q+=($x $((y+1)) 'v') # first, visit below
   elif [[ $c == '^' ]]; then
-    q+=($x $y '^')
-    q+=($((x+1)) $y 'v')
-    q+=($((x-1)) $y 'v')
-  elif [[ $c == '|' ]]; then
-    v=${cache[$x,$y]}
-    # print_grid $x $y 31 1>&2
-    # ((count+=v))
+    q+=($x $y '^') # third, revisit ourself
+    q+=($((x+1)) $y 'v') # second, go right
+    q+=($((x-1)) $y 'v') # first, go left
   fi
 done
-# print_grid
+
 echo ${cache[$sx,$sy]}
