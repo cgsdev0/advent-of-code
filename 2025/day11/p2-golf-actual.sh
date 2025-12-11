db5dafb08cefcf8700efc9554ae2a3f69415cb2a
Sarah Schulte <git@sarah.engineer>
2025-12-11 03:33:03 -0800

    golf fr

---
+++ 2025/day11/p2-golf-actual.sh
@@ -0,0 +1,6 @@
+# requries bash 5.3+
+declare -A c;i=$1
+f(){ local h=$1$2$3 fft=$2 dac=$3;[ $1 = out ]&&
+c[$h]=$[$2&&$3]||[ -z ${c[$h]} ]&&{ eval "(($1++))"
+for x in `grep ^$1 $i|tail -c+6`;do((c[$h]+=${
+f $x $fft $dac;}))done };echo $[c[$h]];};f svr 0 0
