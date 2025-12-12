24a9e2a06aab6bea3157632d41f46e39f83feb7b
Sarah Schulte <git@sarah.engineer>
2025-12-12 06:06:11 -0800

    noisy edition

--- 2025/day11/p2-golf-actual.sh
+++ 2025/day11/p2-golf-actual.sh
@@ -2,6 +2,6 @@
 # ./p2-golf-actual.sh [input]
 declare -A c;i=$1;f(){
 local h=$1$2 z=$2;[ $1 = out ]&&c[$h]=$[z>1]||
-[ ! ${c[$h]} ]&&{ [[ $1 =~ fft|ac ]]&&$[z++]
+${c[$h]}&&{ [[ $1 =~ fft|ac ]]&&$[z++]
 for x in `grep ^$1 $i|tail -c+6`;do((c[$h]+=${
 f $x $z;}))done };echo $[c[$h]];};f svr
