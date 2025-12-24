382f93556e437fe2bb14cbbf7e7e48b237ddcaa3
Sarah Schulte <git@sarah.engineer>
2025-12-23 21:54:20 -0800

    how did it even working

--- synacor/vm.sh
+++ synacor/vm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd saves
+
 mapfile -t mem < <(xxd -c2 -e challenge.bin | cut -d' ' -f2)
 for ((i=0; i<${#mem[@]}; i++)); do
   mem[i]=$((16#${mem[i]}))
@@ -7,19 +9,76 @@ done
 reg=(0 0 0 0 0 0 0 0)
 stack=()
 pc=0
+input=
+has_input=
+
+load() {
+  read pc
+  read -a reg
+  read stack_size
+  unset stack
+  declare -ga stack
+  for((i=0; i<stack_size; i++)); do
+    read num
+    stack+=($num)
+  done
+  read mem_size
+  unset mem
+  declare -ga mem
+  for((i=0; i<=mem_size; i++)); do
+    read num
+    mem+=($num)
+  done
+}
+
+if [[ -n "$1" ]]; then
+  load < "$1"
+fi
+
+save() {
+  echo "$pc"
+  echo "${reg[@]}"
+  echo "${#stack[@]}"
+  printf '%s\n' "${stack[@]}"
+  keys=("${!mem[@]}")
+  mem_size="${keys[-1]}"
+  echo "$mem_size"
+  for((i=0; i<=mem_size; ++i)); do
+    echo "${mem[$i]:-0}"
+  done
+}
+
+run_cmd() {
+  case "$1" in
+    save)
+      echo "saving to $2..."
+      save > "$2"
+      echo "saved!"
+      ;;
+    load)
+      echo "loading $2..."
+      load < "$2"
+      echo "loaded!"
+      ;;
+    *)
+      echo "unknown command '$1'"
+      ;;
+  esac
+}
+
 step() {
   op=${mem[pc]}
-  va=${mem[pc+1]}
+  va=${mem[pc+1]:-0}
   ra=$((va-32768))
   if [[ $va -gt 32767 ]]; then
     va=${reg[ra]}
   fi
-  vb=${mem[pc+2]}
+  vb=${mem[pc+2]:-0}
   rb=$((vb-32768))
   if [[ $vb -gt 32767 ]]; then
     vb=${reg[rb]}
   fi
-  vc=${mem[pc+3]}
+  vc=${mem[pc+3]:-0}
   rc=$((vc-32768))
   if [[ $vc -gt 32767 ]]; then
     vc=${reg[rc]}
@@ -52,14 +111,14 @@ step() {
       ;;
     6) # jmp
       pc=$va ;;
-    7) #jt
+    7) # jt
       if [[ $va -ne 0 ]]; then
         pc=$vb
       else
         ((pc+=3))
       fi
       ;;
-    8) #jf
+    8) # jf
       if [[ $va -eq 0 ]]; then
         pc=$vb
       else
@@ -117,14 +176,28 @@ step() {
       ((pc+=2))
       ;;
     20) # in
-      IFS= read -rn1 char
-      eval "printf -v dec \"%d\n\" \'""$char"
-      if [[ "$char" == " " ]]; then
-        reg[ra]=32
-      elif [[ $dec -eq 0 ]]; then
-        reg[ra]=10
+      if [[ -z "$has_input" ]]; then
+        input='/'
+        while [[ "${input:0:1}" == "/" ]]; do
+          IFS= read -re input
+          if [[ "${input:0:1}" == "/" ]]; then
+            run_cmd ${input:1}
+          fi
+        done
+        has_input=1
+      fi
+      if [[ "${#input}" -gt 0 ]]; then
+        char="${input:0:1}"
+        input="${input:1}"
+        printf -v dec '%d' "'$char"
+        if [[ "$char" == " " ]]; then
+          reg[ra]=32
+        else
+          reg[ra]=$dec
+        fi
       else
-        reg[ra]=$dec
+        has_input=
+        reg[ra]=10 # enter
       fi
       ((pc+=2))
       ;;
