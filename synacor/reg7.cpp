2e8f95fb9e449a138af786379c16ab9b96065032
Sarah Schulte <git@sarah.engineer>
2025-12-29 01:25:58 -0800

    faster, thanks stewsquared

--- synacor/reg7.cpp
+++ synacor/reg7.cpp
@@ -1,36 +1,30 @@
 #include <stdio.h>
 #include <unordered_map>
 
-struct Key {
-  int r0;
-  int r1;
-
-  bool operator==(const Key &other) const {
-    return (this->r0 == other.r0) && (this->r1 == other.r1);
-  }
-};
-
-template <> struct std::hash<Key> {
-  std::size_t operator()(const Key &c) const {
-    return std::hash<int>()(c.r0) ^ std::hash<int>()(c.r1);
-  }
-};
-
-std::unordered_map<Key, int> cache;
+std::unordered_map<int, int> cache;
 
 int f6049(int r0, int r1, int r7) {
-  Key k{.r0 = r0, .r1 = r1};
+  int k = r0 << 16 ^ r1;
   auto it = cache.find(k);
   if (it != cache.end()) {
     return it->second;
   }
-
-  if (!r0) {
+  if (r0 == 0) {
     int ans = (r1 + 1) % 32768;
     cache.emplace(k, ans);
     return ans;
   }
-  if (!r1) {
+  if (r0 == 1) {
+    int ans = (r7+r1+1)  % 32768;
+    cache.emplace(k, ans);
+    return ans;
+  }
+  if (r0 == 2) {
+    int ans = ((r7+1)*r1 + (r7*2 + 1))  % 32768;
+    cache.emplace(k, ans);
+    return ans;
+  }
+  if (r1 == 0) {
     int ans = f6049((r0 + 32767) % 32768, r7, r7);
     cache.emplace(k, ans);
     return ans;
@@ -43,7 +37,7 @@ int f6049(int r0, int r1, int r7) {
 }
 
 int main() {
-  for (int i = 1; i <= 32768; i++) {
+  for (int i = 32768; i > 1; i--) {
     cache.clear();
     int reg_0 = f6049(4, 1, i);
     printf("trying %d...\n", i);
